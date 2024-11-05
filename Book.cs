5fc5029dd4e8524a155789ef620604a3337af613
syntrice <76839403+Syntrice@users.noreply.github.com>
2024-11-05 15:38:16 +0000

    Create Book class with Title and Page information

---
+++ Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSBasics
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public int Pages { get; set; }
+
+        public Book(string title, int pages)
+        {
+            Title = title;
+            Pages = pages;
+        }
+    }
+}
